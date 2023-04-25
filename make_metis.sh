1f5a8d61a451e370c12de7f42094d37d244b04b0
Jan Blechta <blechta@karlin.mff.cuni.cz>
2023-04-25 12:47:43 +0200

    Fix link rot on METIS's website

--- make_metis.sh
+++ make_metis.sh
@@ -3,12 +3,13 @@ set -e
 
 # Default values
 : ${METIS_VERSION:=5.1.0}
+: ${METIS_WAYBACK_MACHINE_TIMESTAMP:=20170712055800}
 : ${PREFIX:="${PWD}"}
 : ${PROCS:=1}
 
 # Download and unpack
 mkdir -p src && cd src
-wget -c -O metis-${METIS_VERSION}.tar.gz http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/metis-${METIS_VERSION}.tar.gz
+wget -c -O metis-${METIS_VERSION}.tar.gz https://web.archive.org/web/${METIS_WAYBACK_MACHINE_TIMESTAMP}id_/glaros.dtc.umn.edu/gkhome/fetch/sw/metis/metis-${METIS_VERSION}.tar.gz
 tar -xzf metis-${METIS_VERSION}.tar.gz
 cd metis-${METIS_VERSION}
 
